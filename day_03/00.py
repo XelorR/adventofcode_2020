04c17ffa661bbbe2a8bc9929e72f3d25636470eb
XelorR <xelor@ya.ru>
2020-12-03 23:54:53 +0000

    refactored to function. Fails on input. To debug

--- day_03/00.py
+++ day_03/00.py
@@ -15,12 +15,16 @@ example_map = """..##.......
 example_map = example_map.split("\n")
 input_map = input_map.split("\n")
 
-position_in_row = 0
-trees = 0
+def count_trees(some_map: list) -> int:
+    position_in_row = 0
+    trees = 0
 
-for row in example_map:
-    if row[position_in_row % len(row)] == "#":
-        trees += 1
-    position_in_row += 3
+    for row in some_map:
+        if row[position_in_row % len(row)] == "#":
+            trees += 1
+        position_in_row += 3
 
-print(trees)
+    return trees
+
+print("Example map:", count_trees(example_map), "met")
+# print("My input map:", count_trees(input_map), "met")
