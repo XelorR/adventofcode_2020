12d8ef47ce1a1d24ef5171c336eb5a3725b2675e
XelorR <xelor@ya.ru>
2020-12-05 21:03:22 +0000

    Refactored to the function

--- day_05/first_part.py
+++ day_05/first_part.py
@@ -1,19 +1,23 @@
 with open("input.txt") as f:
     INPUT = [row.rstrip() for row in f.readlines()]
 
-low = 0
-high = 127
-left = 0
-right = 7
-for index, letter in enumerate("FBFBBFFRLR"):
-    if letter == "F":
-        high = (high - low) // 2 + low
-    elif letter == "B":
-        low = (high - low) // 2 + low + 1
-    elif letter == "L":
-        right = (right - left) // 2 + left
-    elif letter == "R":
-        left = (right - left) // 2 + left + 1
-    print(letter, low, high, left, right)
-decoded = high * 8 + right
-print("Decoded:", decoded)
+
+def decode_ticket(ticket: str) -> int:
+    low = 0
+    high = 127
+    left = 0
+    right = 7
+
+    for index, letter in enumerate(ticket):
+        if letter == "F":
+            high = (high - low) // 2 + low
+        elif letter == "B":
+            low = (high - low) // 2 + low + 1
+        elif letter == "L":
+            right = (right - left) // 2 + left
+        elif letter == "R":
+            left = (right - left) // 2 + left + 1
+    return high * 8 + right
+
+
+print(decode_ticket("FBFBBFFRLR"))
