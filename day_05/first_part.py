1d970f8f8cb112e3cf5b08cb5addc7ba50e8b04b
XelorR <xelor@ya.ru>
2020-12-05 21:00:05 +0000

    full message decode prototype implemented
    Result matches the example

--- day_05/first_part.py
+++ day_05/first_part.py
@@ -3,13 +3,17 @@ with open("input.txt") as f:
 
 low = 0
 high = 127
+left = 0
+right = 7
 for index, letter in enumerate("FBFBBFFRLR"):
-    print(low, high)
     if letter == "F":
         high = (high - low) // 2 + low
     elif letter == "B":
         low = (high - low) // 2 + low + 1
     elif letter == "L":
-        pass
+        right = (right - left) // 2 + left
     elif letter == "R":
-        pass
+        left = (right - left) // 2 + left + 1
+    print(letter, low, high, left, right)
+decoded = high * 8 + right
+print("Decoded:", decoded)
