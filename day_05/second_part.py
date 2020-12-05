8662601e55026f4e15f87e5c4fe499bf9fcf6c03
XelorR <xelor@ya.ru>
2020-12-05 21:26:27 +0000

    print function for further debug purposes

--- day_05/second_part.py
+++ day_05/second_part.py
@@ -1,8 +1,10 @@
+from typing import List, Union, Any
+
 with open("input.txt") as f:
     INPUT = [row.rstrip() for row in f.readlines()]
 
 
-def decode_ticket(ticket: str) -> int:
+def decode_ticket(ticket: str) -> List[Union[int, Any]]:
     low = 0
     high = 127
     left = 0
@@ -17,7 +19,15 @@ def decode_ticket(ticket: str) -> int:
             right = (right - left) // 2 + left
         elif letter == "R":
             left = (right - left) // 2 + left + 1
-    return high * 8 + right
+    return [high, right, high * 8 + right]
+
+
+def print_decoded_ticket(ticket_to_print):
+    row, column, seat_id = decode_ticket(ticket_to_print)
+    print(f"Ticket: {ticket_to_print}, row: {row}, column: {column}, seat ID: {seat_id}")
+
+
+print_decoded_ticket("BFFFBBFRRR")
 
 
 def generate_fullist():
