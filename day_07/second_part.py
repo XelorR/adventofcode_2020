675b7d71f3dac762326e55385f2e1353e2906a42
XelorR <xelor@ya.ru>
2020-12-07 21:54:37 +0000

    unnecessary code removed

--- day_07/second_part.py
+++ day_07/second_part.py
@@ -1,15 +1,4 @@
 import re
-from pprint import pprint
-
-EXAMPLE = """light red bags contain 1 bright white bag, 2 muted yellow bags.
-dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-bright white bags contain 1 shiny gold bag.
-muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-faded blue bags contain no other bags.
-dotted black bags contain no other bags."""
 
 with open("input.txt") as input_file:
     INPUT = input_file.read()
@@ -36,8 +25,5 @@ def count_in(data: dict, looking_for: str):
     return bag_count
 
 
-example_data = parse_raw(EXAMPLE)
 input_data = parse_raw(INPUT)
-
-pprint(example_data)
 print("Answer is", count_in(input_data, "shiny gold") - 1)
