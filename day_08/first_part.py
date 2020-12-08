c6188cab3a332116dfb28c7199ea6f5b0a529b2f
XelorR <xelor@ya.ru>
2020-12-09 00:37:10 +0300

    loader class implemented, puzzle part one solved

--- day_08/first_part.py
+++ day_08/first_part.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 with open("input.txt", "r") as input_file:
     INPUT = input_file.read().strip()
 
@@ -21,4 +19,61 @@ def parse_raw(data_raw: str) -> list:
 example_data = parse_raw(EXAMPLE)
 input_data = parse_raw(INPUT)
 
-pprint(example_data)
+
+class BootLoader:
+    def __init__(self, data: list):
+        self.data = data
+        self.position = 0
+        self.accumulator = 0
+        self.commands = {
+            "acc": self.__acc,
+            "jmp": self.__jmp,
+            "nop": self.__nop,
+        }
+        self.executed_positions = []
+
+    def __acc(self, change_accumulator_by: int):
+        """
+        increases or decreases a single global value called the accumulator by the value given in
+        the argument. For example, acc +7 would increase the accumulator by 7. The accumulator
+        starts at 0. After an acc instruction, the instruction immediately below it is executed
+        """
+        self.accumulator += change_accumulator_by
+        self.position = (self.position + 1) % len(self.data)
+
+    def __jmp(self, jump: int):
+        """
+        jumps to a new instruction relative to itself. The next instruction to execute is found
+        using the argument as an offset from the jmp instruction; for example, jmp +2 would skip
+        the next instruction, jmp +1 would continue to the instruction immediately below it,
+        and jmp -20 would cause the instruction 20 lines above to be executed next
+        """
+        self.position = (self.position + jump) % len(self.data)
+
+    def __nop(self, nope: int):
+        """
+        stands for No Operation - it does nothing. The instruction immediately below it is
+        executed next
+        """
+        self.position = (self.position + 1) % len(self.data)
+
+    def execute(self):
+        for _ in range(len(self.data)):
+            comm_name = self.data[self.position][0]
+            argument = self.data[self.position][-1]
+            func = self.commands[comm_name]
+
+            if self.position in self.executed_positions:
+                break
+            else:
+                self.executed_positions.append(self.position)
+            func(argument)
+
+
+example_loader = BootLoader(parse_raw(EXAMPLE))
+example_loader.execute()
+print(example_loader.accumulator)
+
+input_loader = BootLoader(parse_raw(INPUT))
+input_loader.execute()
+print(input_loader.accumulator)
