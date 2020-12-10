4cfc89eea1c240d07a450a1ca55bcc26a55fa672
XelorR <xelor@ya.ru>
2020-12-10 20:27:44 +0300

    first part solution done

--- day_09/first_part.py
+++ day_09/first_part.py
@@ -25,3 +25,19 @@ EXAMPLE = """35
 
 def parse_raw(data_raw: str) -> list:
     return [int(e) for e in data_raw.splitlines()]
+
+
+def find_first_error(data_raw, length_to_check=5):
+    for index, elem in enumerate(parse_raw(data_raw)[length_to_check:]):
+        prev_five = parse_raw(data_raw)[index: index + length_to_check]
+        check = False
+        for i, a in enumerate(prev_five):
+            for j, b in enumerate(prev_five):
+                if i != j and a + b == elem:
+                    check = True
+        if not check:
+            return elem
+
+
+assert find_first_error(EXAMPLE) == 127
+print(find_first_error(INPUT, 25))
