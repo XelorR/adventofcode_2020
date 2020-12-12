b9193ac155aa0efd9f869dbab0436c9091451fb9
XelorR <xelor@ya.ru>
2020-12-12 10:40:40 +0300

    day 11 inputs added

---
+++ day_11/solution.py
@@ -0,0 +1,13 @@
+EXAMPLE = """L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL"""
+
+with open("input.txt") as input_file:
+    INPUT = input_file.read().strip()
