660894b585126095f95a465124025a0a2b695b23
XelorR <xelor@ya.ru>
2020-12-16 01:01:16 +0300

    making method public

--- day_11/solution.py
+++ day_11/solution.py
@@ -19,25 +19,25 @@ class Ferry:
         self.width = len(self.data[0])
         self.length = len(self.data)
 
-    def __get_seats_left(self, row: int, column: int) -> list:
+    def get_seats_left(self, row: int, column: int) -> list:
         if column <= 8:
             return self.data[row][:column]
         else:
             return self.data[row][column - 8:column]
 
-    def __get_seats_right(self, row: int, column: int) -> list:
+    def get_seats_right(self, row: int, column: int) -> list:
         if self.width - column <= 9:
             return self.data[row][column + 1:]
         else:
             return self.data[row][column + 1:column + 9]
 
-    def __get_seats_up(self, row: int, column: int) -> list:
+    def get_seats_up(self, row: int, column: int) -> list:
         if row <= 8:
             return [seat[column] for seat in self.data[:row]]
         else:
             return [seat[column] for seat in self.data[row - 8:row]]
 
-    def __get_seats_down(self, row: int, column: int) -> list:
+    def get_seats_down(self, row: int, column: int) -> list:
         if self.length - row <= 9:
             return [seat[column] for seat in self.data[row + 1:]]
         else:
