88a3e39a1e0dfbab639e8e8eb762c56b96573819
XelorR <xelor@ya.ru>
2020-12-16 00:15:13 +0300

    data parsed into class constructor

--- day_11/solution.py
+++ day_11/solution.py
@@ -11,3 +11,10 @@ L.LLLLL.LL"""
 
 with open("input.txt") as input_file:
     INPUT = input_file.read().strip()
+
+
+class Ferry:
+    def __init__(self, data_raw: str):
+        self.data = [[position for position in row] for row in data_raw.splitlines()]
+        self.width = len(self.data[0])
+        self.length = len(self.data)
