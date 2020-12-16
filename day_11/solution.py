e9879d65a4878e3bb3cd9701ca71d8b762b56809
XelorR <xelor@ya.ru>
2020-12-16 16:54:14 +0300

    one more diagonal method implemented

--- day_11/solution.py
+++ day_11/solution.py
@@ -58,6 +58,18 @@ class Ferry:
             current_column -= 1
         return seats
 
+    def get_seats_down_right(self, row: int, column: int) -> list:
+        seats: List[str] = []
+        current_row = row + 1
+        current_column = column + 1
+        while True:
+            if current_row == self.length or current_column == self.width:
+                break
+            seats.append(self.data[current_row][current_column])
+            current_row += 1
+            current_column += 1
+        return seats
+
 
 example_ferry = Ferry(EXAMPLE)
 pprint(example_ferry.data)
