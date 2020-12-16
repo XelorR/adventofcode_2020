c2bd84975dd3b6dad5084e643dca4534f08e304d
XelorR <xelor@ya.ru>
2020-12-16 18:32:31 +0300

    shit removed

--- day_11/solution.py
+++ day_11/solution.py
@@ -18,27 +18,3 @@ class Ferry:
         self.data = [[position for position in row] for row in data_raw.splitlines()]
         self.width = len(self.data[0])
         self.length = len(self.data)
-
-    def get_seats_left(self, row: int, column: int) -> list:
-        if column <= 8:
-            return self.data[row][:column]
-        else:
-            return self.data[row][column - 8:column]
-
-    def get_seats_right(self, row: int, column: int) -> list:
-        if self.width - column <= 9:
-            return self.data[row][column + 1:]
-        else:
-            return self.data[row][column + 1:column + 9]
-
-    def get_seats_up(self, row: int, column: int) -> list:
-        if row <= 8:
-            return [seat[column] for seat in self.data[:row]]
-        else:
-            return [seat[column] for seat in self.data[row - 8:row]]
-
-    def get_seats_down(self, row: int, column: int) -> list:
-        if self.length - row <= 9:
-            return [seat[column] for seat in self.data[row + 1:]]
-        else:
-            return [seat[column] for seat in self.data[row + 1:row + 9]]
