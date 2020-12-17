6b6f8e903eb8758ea321adac4dcd07de388e9713
XelorR <xelor@ya.ru>
2020-12-17 19:45:01 +0300

    type hints added

--- day_11/solution.py
+++ day_11/solution.py
@@ -17,6 +17,11 @@ with open("input.txt") as input_file:
 
 
 class Ferry:
+    data: list
+    width: int
+    length: int
+    occupied: int
+
     def __init__(self, data_raw: str):
         self.data = [[position for position in row] for row in data_raw.splitlines()]
         self.width = len(self.data[0])
