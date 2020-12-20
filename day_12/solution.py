61a1625bbbd896202981609143f2519bfc785ce7
XelorR <xelor@ya.ru>
2020-12-20 07:19:17 +0300

    ship creation started

--- day_12/solution.py
+++ day_12/solution.py
@@ -1,3 +1,5 @@
+import re
+
 EXAMPLE = """F10
 N3
 F7
@@ -6,3 +8,11 @@ F11"""
 
 with open("input.txt", "r") as input_file:
     INPUT = input_file.read().strip()
+
+
+class Ship:
+    def __init__(self, raw_data: str):
+        self.data = (re.findall(r"(\w+)(\d+)", comm)[0] for comm in raw_data.splitlines())
+
+
+example_ship = Ship(EXAMPLE)
