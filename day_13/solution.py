689ae76ed5a53cfa21c0ebcfacc0dfdc9706146d
XelorR <xelor@ya.ru>
2020-12-21 18:23:55 +0300

    reader implemented

--- day_13/solution.py
+++ day_13/solution.py
@@ -3,3 +3,12 @@ EXAMPLE = """939
 
 with open("input.txt") as f:
     INPUT = f.read().strip()
+
+
+def parse_raw(data_raw: str):
+    timestamp, bus_list = data_raw.splitlines()
+    return {
+        "timestamp": int(timestamp),
+        "bus_list": [int(b) for b in bus_list.split(",") if
+                     b != "x"]
+    }
