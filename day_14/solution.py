c17927295397d16f18e9db548105b2e368bd09d0
XelorR <xelor@ya.ru>
2020-12-23 10:14:55 +0300

    input data parser inplemented

--- day_14/solution.py
+++ day_14/solution.py
@@ -1,3 +1,5 @@
+import re
+
 EXAMPLE = """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
@@ -5,3 +7,11 @@ mem[8] = 0"""
 
 with open("input.txt") as f:
     INPUT = f.read().strip()
+
+
+def parse_raw(data_raw: str):
+    data = [c.split(" = ") for c in data_raw.splitlines()]
+    return [
+        c if c[0] == "mask" else [int(re.findall(r"\d+", c[0])[0]), int(c[1])]
+        for c in data
+    ]
